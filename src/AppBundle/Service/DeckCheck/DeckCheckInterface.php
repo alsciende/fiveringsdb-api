a32429cc630e67ed11d178a6f23d81c66dc20a52
Alsciende <bertolini.cedric@me.com>
2017-07-04 12:36:27 +0200

    unit tests for deck check services
    turn card.keywords into an array of codes
    
    fix UI in mobile

--- src/AppBundle/Service/DeckCheck/DeckCheckInterface.php
+++ src/AppBundle/Service/DeckCheck/DeckCheckInterface.php
@@ -1,13 +1,13 @@
 <?php
 
-namespace AppBundle\Service\DeckChecker;
+namespace AppBundle\Service\DeckCheck;
 
 use AppBundle\Model\CardSlotCollectionDecorator;
 
 /**
  * @author Alsciende <alsciende@icloud.com>
  */
-interface DeckCheckerInterface
+interface DeckCheckInterface
 {
     public function check(CardSlotCollectionDecorator $deckCards): int;
 }
