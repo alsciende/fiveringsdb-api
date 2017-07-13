e0cdbf9271cc2e148a97bb797aa7bf13321f0755
Alsciende <bertolini.cedric@me.com>
2017-07-13 16:44:31 +0200

    removed /api/v1 routes

--- tests/AppBundle/Controller/BaseApiControllerTest.php
+++ tests/AppBundle/Controller/BaseApiControllerTest.php
@@ -5,31 +5,32 @@ declare(strict_types=1);
 namespace Tests\AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Client;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 /**
  * Description of BaseApiControllerTest
  *
  * @author Cédric Bertolini <cedric.bertolini@proximedia.fr>
  */
-abstract class BaseApiControllerTest extends \Tests\AppBundle\Controller\BaseControllerTest
+abstract class BaseApiControllerTest extends WebTestCase
 {
+    public function getAnonymousClient (): Client
+    {
+        return static::createClient();
+    }
+
+    public function getAuthenticatedClient ($username = 'user', $password = 'user'): Client
+    {
+        return static::createClient(array(), array(
+                    'HTTP_X-Access-Token' => $username."-access-token",
+        ));
+    }
 
     public function getContent (Client $client)
     {
         return json_decode($client->getResponse()->getContent(), true);
     }
 
-    /**
-     * 
-     * @return Client
-     */
-    public function getAuthenticatedClient ($username = 'user', $password = 'user')
-    {
-        return static::createClient(array(), array(
-                    'HTTP_X-Access-Token' => $username."-access-token",
-        ));
-    }
-    
     public function sendJsonRequest(\Symfony\Component\BrowserKit\Client $client, string $method, string $uri, array $data = [])
     {
         $client->request($method, $uri, [], [], [], json_encode($data));
