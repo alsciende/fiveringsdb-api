eedec99e8c15c889d022a2831e6e0b4065e9d794
Alsciende <bertolini.cedric@me.com>
2017-07-21 13:51:51 +0200

    wip data transformer for deck cards

--- src/AppBundle/Form/Type/ReviewType.php
+++ src/AppBundle/Form/Type/ReviewType.php
@@ -2,28 +2,27 @@
 
 declare(strict_types=1);
 
-namespace AppBundle\Form;
+namespace AppBundle\Form\Type;
 
-use AppBundle\Entity\Deck;
+use AppBundle\Entity\Review;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class DeckType extends AbstractType
+class ReviewType extends AbstractType
 {
     public function buildForm (FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('name', TextType::class)
-            ->add('description', TextType::class)
-            ;
+            ->add('text', TextType::class)
+        ;
     }
 
     public function configureOptions (OptionsResolver $resolver)
     {
         $resolver->setDefaults([
-            'data_class' => Deck::class,
+            'data_class' => Review::class,
             'csrf_protection' => false,
         ]);
     }
