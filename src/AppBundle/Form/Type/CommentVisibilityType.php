1baecc7b42c7c54ae609b2468c135fec8f1ada59
Alsciende <bertolini.cedric@me.com>
2017-07-23 15:53:45 +0200

    security for deck comment visibility

--- src/AppBundle/Form/Type/CommentVisibilityType.php
+++ src/AppBundle/Form/Type/CommentVisibilityType.php
@@ -6,16 +6,16 @@ namespace AppBundle\Form\Type;
 
 use AppBundle\Entity\Comment;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class CommentType extends AbstractType
+class CommentVisibilityType extends AbstractType
 {
     public function buildForm (FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('text', TextType::class)
+            ->add('visible', CheckboxType::class)
             ;
     }
 
