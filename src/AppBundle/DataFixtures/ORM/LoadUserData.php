0962c833a2d490f83862fbf9af34b4f03af64d1f
Alsciende <bertolini.cedric@me.com>
2017-08-05 18:34:47 +0200

    fix use

--- src/AppBundle/DataFixtures/ORM/LoadUserData.php
+++ src/AppBundle/DataFixtures/ORM/LoadUserData.php
@@ -2,7 +2,7 @@
 
 namespace AppBundle\DataFixtures\ORM;
 
-use Alsciende\SecurityBundle\Service\UserManager;
+use AppBundle\Service\UserManager;
 use Doctrine\Common\DataFixtures\AbstractFixture;
 use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
