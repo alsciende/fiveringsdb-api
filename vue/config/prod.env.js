8509628ba4ede86bff063dfcae16f25265f7f22f
Jens Böttcher <eljenso.boettcher@gmail.com>
2017-09-14 13:26:18 +0200

    Make config files linter-happy

--- vue/config/prod.env.js
+++ vue/config/prod.env.js
@@ -2,4 +2,4 @@ module.exports = {
   NODE_ENV: '"production"',
   API_URL: JSON.stringify(process.env.FIVERINGSDB_API_URL),
   GA_TRACKING_ID: JSON.stringify(process.env.FIVERINGSDB_GA_TRACKING_ID),
-}
+};
