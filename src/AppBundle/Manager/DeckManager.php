450ab8ce699cb345e817034b4ce6cb1dc10cd77e
Alsciende <bertolini.cedric@me.com>
2017-10-08 13:31:54 +0200

    DeckListManager and getLike

--- src/AppBundle/Manager/DeckManager.php
+++ src/AppBundle/Manager/DeckManager.php
@@ -99,38 +99,4 @@ class DeckManager
     {
         return $this->entityManager->getRepository(Strain::class)->countBy(['user' => $user]);
     }
-
-    /**
-     * Add a like for this deck and user.
-     * If the like was added, return the new number of likes.
-     * Else return null.
-     */
-    public function addLike (Deck $deck, User $user): ?int
-    {
-        $deckLike = $this->entityManager->getRepository(DeckLike::class)->findOneBy(['deck' => $deck, 'user' => $user]);
-        if ($deckLike) {
-            return null;
-        }
-        $this->entityManager->persist(new DeckLike($deck, $user));
-        $deck->setNbLikes($deck->getNbLikes() + 1);
-
-        return $deck->getNbLikes();
-    }
-
-    /**
-     * Remove a like for this deck and user.
-     * If the like was removed, return the new number of likes.
-     * Else return null.
-     */
-    public function removeLike (Deck $deck, User $user): ?int
-    {
-        $deckLike = $this->entityManager->getRepository(DeckLike::class)->findOneBy(['deck' => $deck, 'user' => $user]);
-        if ($deckLike === null) {
-            return null;
-        }
-        $this->entityManager->remove($deckLike);
-        $deck->setNbLikes($deck->getNbLikes() - 1);
-
-        return $deck->getNbLikes();
-    }
 }
