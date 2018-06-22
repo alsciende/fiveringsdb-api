895c6f2ba672da80b6e00d4b89e744bd06689732
Alsciende <bertolini.cedric@me.com>
2018-06-22 18:37:09 +0200

    command to create a PackCard json file from FFG data

--- src/AppBundle/Command/DataFetchCommand.php
+++ src/AppBundle/Command/DataFetchCommand.php
@@ -2,10 +2,14 @@
 
 namespace AppBundle\Command;
 
+use AppBundle\Entity\Card;
 use AppBundle\Entity\Pack;
+use Doctrine\Common\Persistence\ObjectRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\Output;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Question\ChoiceQuestion;
 
@@ -16,27 +20,117 @@ use Symfony\Component\Console\Question\ChoiceQuestion;
  */
 class DataFetchCommand extends Command
 {
-    /** @var EntityManagerInterface $entityManager */
+    /**
+     * @var EntityManagerInterface
+     */
     private $entityManager;
 
-    public function __construct ($name = null, EntityManagerInterface $entityManager)
+    /**
+     * @var ObjectRepository
+     */
+    private $cardRepository;
+
+    /**
+     * @var string
+     */
+    private $imageBaseURL;
+
+    /**
+     * @var ObjectRepository
+     */
+    private $packRepository;
+
+    public function __construct($name = null, EntityManagerInterface $entityManager)
     {
         parent::__construct($name);
         $this->entityManager = $entityManager;
+        $this->cardRepository = $this->entityManager->getRepository(Card::class);
+        $this->packRepository = $this->entityManager->getRepository(Pack::class);
+        $this->imageBaseURL = "http://lcg-cdn.fantasyflightgames.com/l5r/";
     }
 
-    protected function configure ()
+    protected function configure()
     {
         $this
             ->setName('app:data:fetch')
-            ->setDescription("Fetch a data file for a pack from cardgamedb.com");
+            ->setDescription("Fetch a data file for a pack from cardgamedb.com")
+            ->addArgument('pack-id', InputArgument::OPTIONAL, 'Id of the pack to fetch');
     }
 
-    protected function execute (InputInterface $input, OutputInterface $output)
+    protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $packRepository = $this->entityManager->getRepository(Pack::class);
+        $id = $input->hasArgument('pack-id') ? $input->getArgument('pack-id') : $this->askPackId($input, $output);
+        $pack = $this->entityManager->find(Pack::class, $id);
+        if (!$pack instanceof Pack) {
+            throw new \RuntimeException('Unknown pack id');
+        }
 
-        $packs = $packRepository->findBy([], ['ffgId' => 'ASC']);
+        $this->writeFile($output, $id, array_map(
+            [$this, 'getPackCard'],
+            $this->filterByPack(
+                $this->getItemsFromFile(
+                    $this->getFileUrl($pack)
+                ),
+                $pack
+            )
+        ));
+    }
+
+    /**
+     * @param string $filename
+     * @return array
+     */
+    protected function getItemsFromFile(string $filename): array
+    {
+        $content = file_get_contents($filename);
+        $json = rtrim(substr($content, 8), ';');
+
+        return json_decode($json, true);
+    }
+
+    /**
+     * @param array $items
+     * @param Pack  $pack
+     * @return array
+     */
+    private function filterByPack(array $items, Pack $pack)
+    {
+        return array_filter($items, function ($item) use ($pack) {
+            return $item['setname'] === $pack->getName();
+        });
+    }
+
+    /**
+     * @param $repository
+     * @param $item
+     */
+    protected function getPackCard($item): array
+    {
+        $name = $item['name'];
+        $name = str_replace("’", "'", $name);
+        $card = $this->cardRepository->findOneBy(['name' => $name]);
+        if ($card instanceof Card) {
+            return [
+                "card_id"     => $card->getId(),
+                "flavor"      => "",
+                "illustrator" => $item['illus'],
+                "image_url"   => $this->imageBaseURL . $item['img'],
+                "position"    => ltrim($item['num'], 0),
+                "quantity"    => 3,
+            ];
+        }
+
+        throw new \RuntimeException(sprintf('Missing card "%s"', $item['name']));
+    }
+
+    /**
+     * @param InputInterface  $input
+     * @param OutputInterface $output
+     * @return mixed
+     */
+    protected function askPackId(InputInterface $input, OutputInterface $output)
+    {
+        $packs = $this->packRepository->findBy([], ['ffgId' => 'ASC']);
         $arrayMap = array_map(
             function (Pack $pack) {
                 return $pack->getId();
@@ -48,14 +142,36 @@ class DataFetchCommand extends Command
             $arrayMap
         );
         $id = $this->getHelper('question')->ask($input, $output, $choiceQuestion);
-        $pack = $this->entityManager->find(Pack::class, $id);
 
-        if ($pack instanceof Pack) {
-            $command = sprintf(
-                'curl -o l5r-db.js http://www.cardgamedb.com/deckbuilders/legendofthefiverings/database/%s-db.jgz',
-                $pack->getFfgId()
-            );
-            exec($command);
-        }
+        return $id;
     }
+
+    /**
+     * @param OutputInterface $output
+     * @param string          $packId
+     * @param array           $packCards
+     */
+    protected function writeFile(OutputInterface $output, string $packId, array $packCards): void
+    {
+        usort($packCards, function ($a, $b) {
+            return $a['position'] <=> $b['position'];
+        });
+
+        $filename = sprintf('%s.json', $packId);
+        file_put_contents($filename, json_encode($packCards, JSON_PRETTY_PRINT));
+        $output->writeln(sprintf('Wrote file %s with %d cards', $filename, count($packCards)));
+    }
+
+    /**
+     * @param Pack $pack
+     * @return string
+     */
+    protected function getFileUrl(Pack $pack): string
+    {
+        return sprintf(
+            'http://www.cardgamedb.com/deckbuilders/legendofthefiverings/database/%s-db.jgz',
+            $pack->getFfgId()
+        );
+    }
+
 }
